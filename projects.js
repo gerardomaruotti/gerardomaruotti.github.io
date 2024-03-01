96bc074fcab06f65a2ba0f639319b0332bfe0032
Gerardo Maruotti <maruotti@icloud.com>
2024-03-01 13:31:32 +0100

    add about section and start fixing colors

--- projects.js
+++ projects.js
@@ -3,43 +3,49 @@
 const projects = [
 	{
 		name: 'ThesisManagement',
-		description:
-			"ThesisManagement revitalizes Politecnico di Torino's thesis system, prioritizing a user-friendly experience. Focus includes improving frontend/backend, modernizing the interface, and streamlining system functions.",
+		description: "Project to upgrade and simplify the Politecnico di Torino's thesis management process",
+		// description:
+		// 	"ThesisManagement revitalizes Politecnico di Torino's thesis system, prioritizing a user-friendly experience. Focus includes improving frontend/backend, modernizing the interface, and streamlining system functions.",
 		technologies: ['React', 'JavaScript', 'Bootstrap', 'Figma', 'Docker', 'Node'],
 		link: 'https://github.com/gerardomaruotti/ThesisManagement',
 	},
 	{
 		name: 'CodeMastery',
-		description:
-			'The project aims to simplify computer science education for non-STEM university students, focusing on mastering programming skills for informatics exams. The goal is to assist students in overcoming challenges related to learning basic computer science concepts and programming skills, enabling them to excel in exams and prepare for a successful professional future aligned with modern demands.',
+		description: 'Project to simplify computer science education and programming for non-STEM university students',
+		// description:
+		// 	'The project aims to simplify computer science education for non-STEM university students, focusing on mastering programming skills for informatics exams. The goal is to assist students in overcoming challenges related to learning basic computer science concepts and programming skills, enabling them to excel in exams and prepare for a successful professional future aligned with modern demands.',
 		technologies: ['React', 'JavaScript', 'Figma', 'Node', 'Tailwind'],
 		link: 'https://github.com/gerardomaruotti/CodeMastery',
 	},
 	{
 		name: 'EZWallet',
-		description:
-			'EZWallet is a web application designed to help individuals and families keep track of their expenses. Users can enter and categorize their expenses, allowing them to quickly see where their money is going. EZWallet is a powerful tool for those looking to take control of their finances and make informed decisions about their spending.',
+		description: 'Web app that helps users track and categorize their expenses and make informed decisions about their spending',
+		// description:
+		// 	'EZWallet is a web application designed to help individuals and families keep track of their expenses. Users can enter and categorize their expenses, allowing them to quickly see where their money is going. EZWallet is a powerful tool for those looking to take control of their finances and make informed decisions about their spending.',
 		technologies: ['Node', 'JavaScript', 'Docker'],
 		link: 'https://github.com/gerardomaruotti/EzWallet',
 	},
 	{
 		name: 'CMSmall',
-		description:
-			'CMSmall is a web application for a small Content Management System. The CMS has a back-office (i.e., the administrative/management area) and a front-office (i.e., the version of the web application visible to everybody, without authentication). In the back-office, the CMS allows any authenticated user to create and manage pages.',
+		description: 'Web application for a small Content Management System to create and manage articles',
+		// description:
+		// 	'CMSmall is a web application for a small Content Management System. The CMS has a back-office (i.e., the administrative/management area) and a front-office (i.e., the version of the web application visible to everybody, without authentication). In the back-office, the CMS allows any authenticated user to create and manage pages.',
 		technologies: ['React', 'JavaScript', 'Bootstrap', 'Node'],
 		link: 'https://github.com/gerardomaruotti/CMSmall',
 	},
 	{
 		name: 'Graph Partitioner',
-		description:
-			'In computer science, it is common to work with large graphs that need to be divided into non-overlapping sections, known as partitions. This process is called p-way partitioning. The aim of the project is to minimize the sum of the weights of edges that cross between partitions and balance the sum of weights of nodes in each partition.',
+		description: 'C++ project to divide large graphs into non-overlapping sections, known as partitions',
+		// description:
+		// 	'In computer science, it is common to work with large graphs that need to be divided into non-overlapping sections, known as partitions. This process is called p-way partitioning. The aim of the project is to minimize the sum of the weights of edges that cross between partitions and balance the sum of weights of nodes in each partition.',
 		technologies: ['C++', 'C'],
 		link: 'https://github.com/gerardomaruotti/Graph-Partitioner',
 	},
 	{
 		name: 'Tamagotchi',
-		description:
-			'LandTiger Tamagotchi is an engaging virtual pet simulation game developed in C and Assembly language, deployed on a LandTiger development board. It combines the classic Tamagotchi concept of nurturing and taking care of a virtual pet with the powerful capabilities of the LandTiger development board.',
+		description: 'Virtual pet simulation game deployed on a LandTiger development board',
+		// description:
+		// 	'LandTiger Tamagotchi is an engaging virtual pet simulation game developed in C and Assembly language, deployed on a LandTiger development board. It combines the classic Tamagotchi concept of nurturing and taking care of a virtual pet with the powerful capabilities of the LandTiger development board.',
 		technologies: ['C', 'ASM'],
 		link: 'https://github.com/gerardomaruotti/Tamagotchi',
 	},
@@ -51,8 +57,9 @@ const projects = [
 	// },
 	{
 		name: 'Temperature Manager',
-		description:
-			'This Python project demonstrates the application of MQTT for data management. It presents an interactive program structure with initialization, user input handling for data collection control, and a termination process.',
+		description: 'Python project that demonstrates the application of MQTT for reading and writing temperature data',
+		// description:
+		// 	'This Python project demonstrates the application of MQTT for data management. It presents an interactive program structure with initialization, user input handling for data collection control, and a termination process.',
 		technologies: ['Python', 'MQTT'],
 		link: 'https://github.com/gerardomaruotti/Temperature-Manager',
 	},
@@ -64,14 +71,6 @@ const projects = [
 	},
 ];
 
-/**
- * Creates a project card element based on the given project object.
- * The card element contains various HTML elements such as divs, headings, icons, and buttons,
- * which are dynamically created and styled based on the project data.
- *
- * @param {Object} project - The project object containing the project details such as name, technologies, description, and link.
- * @returns {HTMLElement} - The dynamically created project card element.
- */
 function createProjectCard(project) {
 	const card = document.createElement('div');
 	card.classList.add(
@@ -158,7 +157,7 @@ function createProjectCard(project) {
 	}
 
 	const headerDiv = document.createElement('div');
-	headerDiv.classList.add('flex', 'justify-between', 'items-center', 'flex-wrap');
+	headerDiv.classList.add('flex', 'justify-between', 'items-center', 'flex-wrap', 'text-sky-950', 'dark:text-sky-100');
 
 	const cardText = document.createElement('p');
 	cardText.classList.add('card-text', 'py-2');
@@ -168,7 +167,7 @@ function createProjectCard(project) {
 	buttonContainer.classList.add('flex', 'justify-center', 'items-center');
 
 	const cardLink = document.createElement('button');
-	cardLink.classList.add('btn', 'bg-sky-600', 'hover:bg-sky-700', 'text-white', 'px-4', 'py-2', 'rounded-full', 'transition');
+	cardLink.classList.add('btn', 'bg-sky-500', 'hover:bg-sky-700', 'text-white', 'px-4', 'py-2', 'shadow', 'rounded-full', 'transition');
 	cardLink.href = project.link;
 	cardLink.textContent = 'View on GitHub';
 
